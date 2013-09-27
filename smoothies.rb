338acf7ba63dcf0c91618bbe6c2b220ebbf42100
Matt Lucas <mattlucas@gmail.com>
2013-09-27 09:14:51 -0400

    added the smoothies and made some changes

---
+++ smoothies.rb
@@ -0,0 +1,43 @@
+# Instructors for the quiz are written inline.
+require 'pry'
+
+# docs you may enjoy
+# https://github.com/pry/pry
+# http://www.ruby-doc.org/core-1.9.3/Hash.html
+# http://ruby-doc.org/core-1.9.3/String.html
+# http://ruby-doc.org/core-1.9.3/Array.html
+
+# Every Morning I make a smoothie with the follow ingredients:
+smoothie_ingredients = {
+  'flax seeds' => '1 tbsp',
+  'chia seeds' => '1 tbsp',
+  'coconut flakes' => '1 tbsp',
+  'spirulina' => '1 tsp',
+  'pumpkin seeds' => '1 tbsp',
+  'oatmeal' => '1/4 cup',
+  'coco powder' => '1 tbsp',
+  'peanut butter' => '1 tbsp',
+  'almonds' => '1/4 cup',
+  'walnuts' => '1/4 cup',
+  'spinach' => '1/4 cup',
+  'greek yogurt' => '1/4 cup',
+  'nutrional yeast' => '1 tbsp',
+  'brussel sprouts' => '1/4 cup',
+  'asparagus' => '1/4 cup',
+  'kale' => '1/4 cup',
+  'brocoli rabe' => '1/4 cup',
+  'blue berries' => '1/4 cup',
+  'chopped banana' => '1/2 cup',
+  'straw berries' => '1/4 cup',
+  'mango' => '1/4 cup',
+  'hemp milk' => '1 cup'
+}
+
+# Write a function called blend.
+# It should take all the smoothie ingredients (not the measurements)
+# chop up and mix all the characters
+# and output a mixed string of characters
+# Be sure to remove the spaces, as we don't want any air bubbles in our smoothie!
+
+def blend(ingredients)
+end
